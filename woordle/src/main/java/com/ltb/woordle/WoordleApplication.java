54d610c0db26462186322c8940dec7c397232aa9
Lucas Brist <lucastbrist@gmail.com>
2025-10-29 17:24:32 -0500

    initial commit

---
+++ woordle/src/main/java/com/ltb/woordle/WoordleApplication.java
@@ -0,0 +1,13 @@
+package com.ltb.woordle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoordleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoordleApplication.class, args);
+	}
+
+}
---
+++ woordle/src/test/java/com/ltb/woordle/WoordleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltb.woordle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoordleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
